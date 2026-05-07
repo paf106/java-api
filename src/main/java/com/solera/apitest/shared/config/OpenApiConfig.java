56dd605b801ad5846237ac109569aaa8c91727f9
pablo <pabloavila106@gmail.com>
2026-05-07 02:02:11 +0200

    chore: add production server and authentication config

--- src/main/java/com/solera/apitest/shared/config/OpenApiConfig.java
+++ src/main/java/com/solera/apitest/shared/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.solera.apitest.shared.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,12 +25,21 @@ import org.springframework.context.annotation.Configuration;
                         url = "https://www.apache.org/licenses/LICENSE-2.0"
                 )
         ),
-        servers = @Server(
+        security = @SecurityRequirement(name = "bearerAuth"),
+        servers = {@Server(
                 url = "http://localhost:8080",
                 description = "Development"
-        )
+        ),
+                @Server(
+                        url = "https://api.pabloavila.dev",
+                        description = "Production"
+                )
+        }
 )
-
-
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT")
 public class OpenApiConfig {
 }
